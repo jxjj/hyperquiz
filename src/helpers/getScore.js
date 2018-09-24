c0b533790f57a883a2165e005905ddfb4d99cab9
James Johnson <johnsojr@gmail.com>
2018-09-23 23:28:12 -0500

    Points

--- src/helpers/getScore.js
+++ src/helpers/getScore.js
@@ -6,5 +6,6 @@ export default state => Object.keys(state.answers).reduce((acc, questionNum) => 
   const choice = getChoiceForQuestion(questionNum, state);
   const correctAnswer = getCorrectAnswerForQuestion(questionNum, state);
   const points = getPointsForQuestion(questionNum, state);
-  return correctAnswer === choice ? acc + points : acc;
+  const runningTotal = correctAnswer === choice ? acc + points : acc - points;
+  return runningTotal;
 }, 0);
