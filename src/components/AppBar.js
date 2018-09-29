2c7517e0636226dcb14370a3ff2feffafeb5ec44
James Johnson <johnsojr@gmail.com>
2018-09-29 16:41:54 -0500

    Add Link to Home

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,4 +1,5 @@
 import { h } from 'hyperapp';
+import { Link } from '@hyperapp/router';
 import logo from '../img/hyperquiz-logo.svg';
 
 import './AppBar.css';
@@ -6,8 +7,10 @@ import './AppBar.css';
 export default () => (
   <header class="app-bar">
     <div class="app-bar__name">
-      <img class="app-bar__logo" src={logo} alt="HyperQuiz Logo" />
-      HyperQuiz
+      <Link to="/">
+        <img class="app-bar__logo" src={logo} alt="HyperQuiz Logo" />
+        HyperQuiz
+      </Link>
     </div>
   </header>
 );
